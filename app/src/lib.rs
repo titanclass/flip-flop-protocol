49beeb6186a0fe28d2378505c94d6d1bbe72cf31
huntc <huntchr@gmail.com>
2021-11-18 11:05:30 +1100

    Formatting

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -5,9 +5,9 @@ use serde::{Deserialize, Serialize};
 
 /// A Command may only be sent by a client, of which there is only one
 /// client on the bus. Command requests take a type that provides their
-/// command; usually an enum. Command requests convey the last [EventReply] 
-/// offset that the client has processed for the associated server. The 
-/// addressing of servers is left to a lower layer e.g. UDP, or a serial-based 
+/// command; usually an enum. Command requests convey the last [EventReply]
+/// offset that the client has processed for the associated server. The
+/// addressing of servers is left to a lower layer e.g. UDP, or a serial-based
 /// transport.
 #[derive(Debug, Deserialize, PartialEq, Serialize)]
 pub struct CommandRequest<C> {
@@ -17,12 +17,12 @@ pub struct CommandRequest<C> {
 
 /// An EventRequest may only be emitted by a server, of which there can be many, and
 /// only in relation to having received a [CommandRequest] from a client. Event requests
-/// take a type that provides their identifier; usually an enum. Event requests convey 
-/// the offset they are associated with. If an offset overflows to zero then it is the 
+/// take a type that provides their identifier; usually an enum. Event requests convey
+/// the offset they are associated with. If an offset overflows to zero then it is the
 /// server's responsibility to convey any important events that the client may need.
-/// It is the client's responsibility to clear state in relation to previous events when 
-/// an offset less than or equal to the one it requested. An event request also conveys a 
-/// delta in time in a form that the client and its servers understand, and relative to 
+/// It is the client's responsibility to clear state in relation to previous events when
+/// an offset less than or equal to the one it requested. An event request also conveys a
+/// delta in time in a form that the client and its servers understand, and relative to
 /// the server's current notion of time.
 #[derive(Debug, Deserialize, PartialEq, Serialize)]
 pub struct EventReply<E> {
@@ -48,10 +48,7 @@ mod tests {
         };
 
         let mut buf = [0; 32];
-        assert_eq!(
-            postcard::to_slice(&request, &mut buf).unwrap(),
-            [0, 0]
-        );
+        assert_eq!(postcard::to_slice(&request, &mut buf).unwrap(), [0, 0]);
         assert_eq!(
             postcard::from_bytes::<CommandRequest<Command>>(&buf).unwrap(),
             CommandRequest {
