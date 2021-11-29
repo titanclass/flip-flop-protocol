449ccdafc1b8ec629e0b3b52a2953aa38d0e6a5f
huntc <huntchr@gmail.com>
2021-11-29 17:46:30 +1100

    Drops the CRC in favour of using a MIC as per AES-CCM

--- data/src/lib.rs
+++ data/src/lib.rs
@@ -1,14 +1,37 @@
 #![cfg_attr(not(test), no_std)]
 #![doc = include_str!("../README.md")]
 
-use crc_any::CRCu8;
 use serde::{Deserialize, Serialize};
 
-/// Indicates where data is sourced from.
-#[derive(Debug, PartialEq)]
+/// Indicates where data is sourced from i.e. its direction.
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
 pub enum DataSource {
     Client,
     Server,
+}
+
+/// There was an error parsing the data frame. Possibly due
+/// to an incompatible data frame version.
+/// was invalid.
+#[derive(Debug, PartialEq)]
+pub struct ParseError {}
+
+/// The haader fields of the data frame.
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct Header {
+    /// The protocol version. Should be 0.
+    pub version: u8,
+    /// The direction of data flow.
+    pub source: DataSource,
+    /// The address of the server 0..31.
+    pub server_address: u8,
+    /// The port of the server 0..3.
+    pub server_port: u8,
+    /// A frame counter for ensuring message authenticity by
+    /// being able to vary a nonce. Should be incremented by
+    /// the message source and is expected to overflow to zero
+    /// after 0x3F (5 bits).
+    pub frame_counter: u32,
 }
 
 /// A data frame encapsulates client and server packets
@@ -16,52 +39,40 @@ pub enum DataSource {
 #[derive(Debug, Deserialize, PartialEq, Serialize)]
 pub struct DataFrame<'a> {
     // Bits as follows:
-    // 0..=1 protocol version - should be 0
-    // 2..=2 source 0 = client, 1 = server
-    // 3..=7 server address
-    // 8..=9 server port
-    // 10..=15 crc
-    header: u16,
-    data: &'a [u8],
+    // 0..=1   protocol version
+    // 2..=2   source 0 = client, 1 = server
+    // 3..=7   server address
+    // 8..=9   server port
+    // 10..=15 frame counter
+    header: u32,
+    // Payload data appended with a Message Integrity Check (MIC).
+    encrypted_payload: &'a [u8],
 }
 
-/// There was an error parsing the data frame. Possibly due
-/// to an incompatible data frame version or the checksum
-/// was invalid.
-#[derive(Debug, PartialEq)]
-pub struct ParseError {}
-
 impl<'a> DataFrame<'a> {
-    /// Create a new dataframe. A CRCu8 computer is provided so that the
-    /// frame's 6 bit CRC can be determined. Note that any bits beyond
-    /// the 6th will be discarded.
-    pub fn new(
-        mut crc_computer: CRCu8,
-        source: DataSource,
-        server_address: u8,
-        server_port: u8,
-        data: &'a [u8],
-    ) -> Self {
-        let source = if source == DataSource::Client { 0 } else { 1 };
-        crc_computer.digest(data);
-        let crc = crc_computer.get_crc();
+    /// Create a new dataframe with an encrypted payload inclusive of its MIC which
+    /// is expected to be appended at the end.
+    pub fn new(header: &'a Header, encrypted_payload: &'a [u8]) -> Self {
+        let source = if header.source == DataSource::Client {
+            0
+        } else {
+            1
+        };
         Self {
             header: (source << 2)
-                | (((server_address as u16) & 0x1F) << 3)
-                | (((server_port as u16) & 0x03) << 8)
-                | ((crc as u16) << 10),
-            data,
+                | (((header.server_address as u32) & 0x1F) << 3)
+                | (((header.server_port as u32) & 0x03) << 8)
+                | (((header.frame_counter as u32) & 0x3F) << 10),
+            encrypted_payload,
         }
     }
 
-    /// Parse the contents of the data frame passing in a 6 bit CRC
-    /// If the data frame version is an incompatible value or the
-    /// CRC check fails then an error is returned. Otherwise, the
-    /// data source, server address, server port and data is retu
-    pub fn parse(
-        &self,
-        mut crc_computer: CRCu8,
-    ) -> Result<(DataSource, u8, u8, &'a [u8]), ParseError> {
+    /// Parse the contents of the data frame.
+    /// If the data frame version is an incompatible value
+    /// then an error is returned. Otherwise, the header
+    /// and encrypted payload (including a MIC at the end)
+    /// are returned.
+    pub fn parse(&self) -> Result<(Header, &'a [u8]), ParseError> {
         let version = self.header & 0x02;
         let source = match (self.header >> 2) & 0x01 {
             0 => Some(DataSource::Client),
@@ -70,14 +81,19 @@ impl<'a> DataFrame<'a> {
         };
         let server_address = (self.header >> 3) & 0x1f;
         let server_port = (self.header >> 8) & 0x03;
-        let header_crc = self.header >> 10;
-        crc_computer.digest(self.data);
-        let crc = crc_computer.get_crc();
+        let frame_counter = (self.header >> 10) & 0x3f;
 
-        match (version, source, header_crc as u8 == crc) {
-            (0, Some(source), true) => {
-                Ok((source, server_address as _, server_port as _, self.data))
-            }
+        match (version, source) {
+            (0, Some(source)) => Ok((
+                Header {
+                    version: 0,
+                    source,
+                    server_address: server_address as _,
+                    server_port: server_port as _,
+                    frame_counter,
+                },
+                self.encrypted_payload,
+            )),
             _ => Err(ParseError {}),
         }
     }
@@ -86,23 +102,91 @@ impl<'a> DataFrame<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use aes::Aes128;
+    use ccm::aead::AeadInPlace;
+    use ccm::aead::{generic_array::GenericArray, NewAead};
+    use ccm::{
+        consts::{U4, U8},
+        Ccm,
+    };
+    use heapless::Vec;
 
     #[test]
-    fn test_command_serde() {
-        let data = b"some data";
+    fn test_command_serialisation() {
+        type AesCcm = Ccm<Aes128, U4, U8>;
 
-        let expected_frame = DataFrame::new(CRCu8::crc6itu(), DataSource::Server, 31, 2, data);
+        let key = GenericArray::from_slice(b"0123456789ABCDEF");
+        let cipher = AesCcm::new(key);
+
+        let header = Header {
+            version: 0,
+            source: DataSource::Server,
+            server_address: 31,
+            server_port: 2,
+            frame_counter: 1,
+        };
+
+        let mut nonce = [0; 8];
+        let _ = postcard::to_slice(&header, &mut nonce).unwrap();
+        let nonce = GenericArray::from_slice(&nonce);
+
+        let payload = b"some data";
+        let mut encrypted_payload: Vec<u8, 128> = Vec::new();
+        let _ = encrypted_payload.extend_from_slice(payload).unwrap();
+        let _ = cipher
+            .encrypt_in_place(nonce, b"", &mut encrypted_payload)
+            .unwrap();
+
+        let expected_frame = DataFrame::new(&header, &encrypted_payload);
         assert_eq!(
             expected_frame,
             DataFrame {
                 //        FEDCBA_98_76543_2_10
-                header: 0b000001_10_11111_1_00_u16,
-                data: b"some data",
+                header: 0b000001_10_11111_1_00,
+                encrypted_payload: &[91, 146, 159, 160, 124, 31, 21, 57, 196, 230, 143, 129, 18],
             }
         );
+    }
 
-        let expected_parts: Result<(DataSource, u8, u8, &[u8]), ParseError> =
-            Ok((DataSource::Server, 31_u8, 2_u8, data));
-        assert_eq!(expected_frame.parse(CRCu8::crc6itu()), expected_parts);
+    #[test]
+    fn test_command_deserialisation() {
+        type AesCcm = Ccm<Aes128, U4, U8>;
+
+        let key = GenericArray::from_slice(b"0123456789ABCDEF");
+        let cipher = AesCcm::new(key);
+
+        let data_frame = DataFrame {
+            //        FEDCBA_98_76543_2_10
+            header: 0b000001_10_11111_1_00,
+            encrypted_payload: &[91, 146, 159, 160, 124, 31, 21, 57, 196, 230, 143, 129, 18],
+        };
+
+        let (header, encrypted_payload) = data_frame.parse().unwrap();
+
+        let expected_header = Header {
+            version: 0,
+            source: DataSource::Server,
+            server_address: 31,
+            server_port: 2,
+            frame_counter: 1,
+        };
+
+        assert_eq!(header, expected_header);
+
+        let mut nonce = [0; 8];
+        let _ = postcard::to_slice(&header, &mut nonce).unwrap();
+        let nonce = GenericArray::from_slice(&nonce);
+
+        let mut decrypted_payload: Vec<u8, 128> = Vec::new();
+        let _ = decrypted_payload
+            .extend_from_slice(encrypted_payload)
+            .unwrap();
+        let _ = cipher
+            .decrypt_in_place(nonce, b"", &mut decrypted_payload)
+            .unwrap();
+
+        let expected_payload = b"some data";
+
+        assert_eq!(decrypted_payload, expected_payload);
     }
 }
